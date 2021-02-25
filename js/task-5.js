a64837592b5dfce22d76b2055ea7118d4488e114
Nadiia Semkiv <48651806+nsem21@users.noreply.github.com>
2021-02-25 20:45:48 +0200

    додано 5 задачу

---
+++ js/task-5.js
@@ -0,0 +1,12 @@
+const userInput = document.querySelector('input');
+const myOutput = document.querySelector('#name-output');
+
+const greeting = (userEvent) => {
+    if (userEvent.target.value) {
+    myOutput.textContent = userEvent.target.value;
+    } else {
+        myOutput.textContent = 'незнакомец';
+    }
+}
+
+userInput.addEventListener('input', greeting);
