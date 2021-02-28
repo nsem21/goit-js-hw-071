1305da49ffeee6f55d710c3698551f11f1e1d8ea
Nadiia Semkiv <48651806+nsem21@users.noreply.github.com>
2021-02-28 19:18:29 +0200

    заміна циклу на рекурсію у 8 задачі

--- js/task-8.js
+++ js/task-8.js
@@ -11,18 +11,36 @@ const createBoxes = (amount) => {
     destroyBoxes();
     let size = 30;
     const itemsList = [];
-       
-    for (let i = 0; i < amount; i += 1){
-        const colorRandom = Math.floor(Math.random()*16777215).toString(16);
-        const item = document.createElement('div');
-        item.style.width = `${size}px`;
-        item.style.height = `${size}px`;
-        item.style.marginBottom = '10px';
-        item.style.backgroundColor = `#${colorRandom}`;
-        itemsList.push(item);
-        size += 10;
+    // recursion, which replaced loop
+    function createDiv (arr, n) {
+        if (n<=0) {
+            return arr;
+        } else {
+            const colorRandom = Math.floor(Math.random()*16777215).toString(16);
+            const box = document.createElement('div');
+            box.style.width = `${size}px`;
+            box.style.height = `${size}px`;
+            box.style.marginBottom = '10px';
+            box.style.backgroundColor = `#${colorRandom}`;
+            size += 10;
+            arr.push(box)
+            return (createDiv(arr,n-1).concat(arr));
+        }
     }
-    boxes.prepend(...itemsList);
+    
+    const newBoxes = createDiv(itemsList, amount); 
+    //loop  
+    //for (let i = 0; i < amount; i += 1){
+    //    const colorRandom = Math.floor(Math.random()*16777215).toString(16);
+    //    const item = document.createElement('div');
+    //    item.style.width = `${size}px`;
+    //    item.style.height = `${size}px`;
+    //    item.style.marginBottom = '10px';
+    //    item.style.backgroundColor = `#${colorRandom}`;
+    //    itemsList.push(item);
+    //    size += 10;
+    //}
+    boxes.prepend(...newBoxes);
 }
 
 const destroyBoxes = () => {
