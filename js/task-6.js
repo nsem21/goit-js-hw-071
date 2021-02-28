bf549539613bf21b41b06bc6d4a5f1d1b76cf7af
Nadiia Semkiv <48651806+nsem21@users.noreply.github.com>
2021-02-28 19:29:39 +0200

    спрощено 6 задачу

--- js/task-6.js
+++ js/task-6.js
@@ -4,13 +4,13 @@ const checkLength = Number(userInput.getAttribute('data-length'));
 const toValidate = (newEvent) => {
     const measurement = newEvent.currentTarget.value.length;
     if (measurement === checkLength) {
-        userInput.classList.remove('valid')
+        //userInput.classList.remove('valid')
         userInput.classList.remove('invalid')
         userInput.classList.add('valid')
 
     } else {
         userInput.classList.remove('valid')
-        userInput.classList.remove('invalid')
+        //userInput.classList.remove('invalid')
         userInput.classList.add('invalid')
     }
 }
